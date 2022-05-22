a563c60d72d9dbc5470968f7c405b57190656155
Guilherme Costa Sales <guicosal@gmail.com>
2022-05-21 23:16:00 -0300

    Business Layer

--- src/main/java/com/example/client/student/StudentService.java
+++ src/main/java/com/example/client/student/StudentService.java
@@ -1,4 +1,19 @@
 package com.example.client.student;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "John Doe",
+                        "john.doe@gmail.com",
+                        LocalDate.of(2002, Month.JANUARY, 4),
+                        20)
+        );
+    }
 }
