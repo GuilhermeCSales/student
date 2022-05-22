4d2208e2ec30d9c00cd17439822298d6e9108a7e
Guilherme Costa Sales <guicosal@gmail.com>
2022-05-22 16:20:36 -0300

    Updating student

--- src/main/java/com/example/client/student/StudentService.java
+++ src/main/java/com/example/client/student/StudentService.java
@@ -2,10 +2,10 @@ package com.example.client.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,9 +34,29 @@ public class StudentService {
     public void deleteStudent(Long studentId) {
         boolean exists = studentRepository.existsById(studentId);
         if (!exists) {
-            throw new IllegalStateException("Student with id" + studentId + "does not exists");
+            throw new IllegalStateException("Student with id " + studentId + " does not exists");
         }
 
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with " + studentId + " does not exist"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("Email already exists");
+            }
+
+            student.setEmail(email);
+        }
     }
 }
