17f47631fbf492d57191c6483af4a26e4114a2ff
Guilherme Costa Sales <guicosal@gmail.com>
2022-05-22 09:05:58 -0300

    Saving Students

---
+++ src/main/java/com/example/client/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.client.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student john = new Student(
+                    "John",
+                    "john@gmail.com",
+                    LocalDate.of(2002, Month.JANUARY, 4),
+                    20
+            );
+
+            Student carolina = new Student(
+                    "Carolina",
+                    "carolina@gmail.com",
+                    LocalDate.of(1983, Month.NOVEMBER, 5),
+                    38
+            );
+
+            repository.saveAll(
+                    List.of(john, carolina)
+            );
+        };
+    }
+}
